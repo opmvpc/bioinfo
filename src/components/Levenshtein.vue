0ad0d9fdb19b2ba3955f97258517e33f44834c3e
opmvpc <thibsix@outlook.be>
2021-06-02 00:24:19 +0200

    trim et lowercase inputs

--- src/components/Levenshtein.vue
+++ src/components/Levenshtein.vue
@@ -53,7 +53,7 @@
         <div class="prose">
           <h2>Résultat</h2>
         </div>
-        <table ref="matrix" >
+        <table ref="matrix">
           <tr v-for="(row, indexRow) in resultMatrix" :key="indexRow">
             <td
               v-for="(col, indexCol) in resultMatrix[indexRow]"
@@ -85,25 +85,27 @@ export default {
       this.animateLevenshtein();
     },
     animateLevenshtein() {
-      this.createMatrix();
-      const operations = this.levensteinIter(this.chaine1, this.chaine2);
+      let chaine1 = this.chaine1.trim().toLowerCase();
+      let chaine2 = this.chaine2.trim().toLowerCase();
+      this.createMatrix(chaine1, chaine2);
+      const operations = this.levensteinIter(chaine1, chaine2);
       setTimeout(() => {
-        this.$refs.matrix.scrollIntoView({behavior: 'smooth'});
+        this.$refs.matrix.scrollIntoView({ behavior: "smooth" });
       }, 200);
       this.renderOperations(operations);
     },
     async renderOperations(operations) {
-      const timer = ms => new Promise(res => setTimeout(res, ms))
+      const timer = ms => new Promise(res => setTimeout(res, ms));
       for (let index = 0; index < operations.length; index++) {
         const operation = operations[index];
-        this.resultMatrix[operation.y + 1][operation.x + 1] = operation.value
+        this.resultMatrix[operation.y + 1][operation.x + 1] = operation.value;
         await timer(this.timer);
       }
     },
-    createMatrix() {
-      let matrix = new Array(this.chaine2.length + 2);
+    createMatrix(chaine1, chaine2) {
+      let matrix = new Array(chaine2.length + 2);
       for (let i = 0; i < matrix.length; i++) {
-        matrix[i] = new Array(this.chaine1.length + 2);
+        matrix[i] = new Array(chaine1.length + 2);
         for (let j = 0; j < matrix[i].length; j++) {
           matrix[i][j] = 0;
         }
@@ -115,21 +117,23 @@ export default {
       matrix[1][0] = "";
 
       for (let index = 2; index < matrix[0].length; index++) {
-        matrix[0][index] = this.chaine1[index - 2];
+        matrix[0][index] = chaine1[index - 2];
       }
 
       for (let i = 2; i < matrix.length; i++) {
-        matrix[i][0] = this.chaine2[i - 2];
+        matrix[i][0] = chaine2[i - 2];
       }
 
       this.resultMatrix = matrix;
     },
     levensteinIter(chaine1, chaine2) {
-      const tailleChaine1 = chaine1.length+1;
-      const tailleChaine2 = chaine2.length+1;
+      const tailleChaine1 = chaine1.length + 1;
+      const tailleChaine2 = chaine2.length + 1;
 
       let operations = [];
-      let matrix = new Array(tailleChaine1).fill(0).map(() => new Array(tailleChaine2).fill(0));
+      let matrix = new Array(tailleChaine1)
+        .fill(0)
+        .map(() => new Array(tailleChaine2).fill(0));
 
       for (let i = 1; i < tailleChaine1; i++) {
         matrix[i][0] = i;
@@ -145,19 +149,19 @@ export default {
       for (let j = 1; j < tailleChaine2; j++) {
         for (let i = 1; i < tailleChaine1; i++) {
           let substitutionCost;
-          if (chaine1.charAt(i-1) === chaine2.charAt(j-1)) {
+          if (chaine1.charAt(i - 1) === chaine2.charAt(j - 1)) {
             substitutionCost = 0;
           } else {
             substitutionCost = 1;
           }
 
           let min = Math.min(
-            matrix[i-1][j] + 1,
-            matrix[i][j-1] + 1,
-            matrix[i-1][j-1] + substitutionCost
+            matrix[i - 1][j] + 1,
+            matrix[i][j - 1] + 1,
+            matrix[i - 1][j - 1] + substitutionCost
           );
           matrix[i][j] = min;
-        operations.push(this.updateCellOperation(i, j, min));
+          operations.push(this.updateCellOperation(i, j, min));
         }
       }
       console.log(operations);
@@ -165,44 +169,43 @@ export default {
     },
     updateCellOperation(xCoord, yCoord, value) {
       return {
-        type: 'update-cell',
+        type: "update-cell",
         x: xCoord,
         y: yCoord,
-        value: value,
-      }
+        value: value
+      };
     },
-    deletionOperation(xCoord, yCoord, value){
+    deletionOperation(xCoord, yCoord, value) {
       return {
-        type: 'delete',
+        type: "delete",
         x: xCoord,
         y: yCoord,
-        value: value,
-      }
+        value: value
+      };
     },
-    insertionOperation(xCoord, yCoord, value){
+    insertionOperation(xCoord, yCoord, value) {
       return {
-        type: 'insertion',
+        type: "insertion",
         x: xCoord,
         y: yCoord,
-        value: value,
-      }
+        value: value
+      };
     },
-    substitutionOperation(xCoord, yCoord, value){
+    substitutionOperation(xCoord, yCoord, value) {
       return {
-        type: 'substitution',
+        type: "substitution",
         x: xCoord,
         y: yCoord,
-        value: value,
-      }
+        value: value
+      };
     }
   }
 };
 </script>
 
 <style lang="postcss" scoped>
-
 table {
-  @apply inline-flex border-4 border-app-200 mt-4
+  @apply inline-flex border-4 border-app-200 mt-4;
 }
 td {
   @apply flex items-center justify-center text-2xl xl:text-4xl border-4 border-app-200 shadow-inner h-12 w-12 xl:h-16 xl:w-16 text-center font-mono;
